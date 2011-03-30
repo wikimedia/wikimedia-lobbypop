48889cab550935cf1fe99dcacd02989155f10b2e
Trevor Parscal <tparscal@users.mediawiki.org>
2011-03-30 01:51:54 +0000

    Mostly working Node.js port of LobbyPop

---
+++ beta/main.js
@@ -0,0 +1,58 @@
+var express = require( 'express' )
+	fs = require( 'fs' )
+	app = express.createServer();
+
+var config = JSON.parse( fs.readFileSync( 'config.json' ) );
+
+app
+	.configure( function() {
+		app
+			.use( '/public', express.static( __dirname + '/public' ) )
+			.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) )
+			.use( express.bodyParser() )
+			.set( 'view engine', 'ejs' );
+	} )
+	.get( '/', function( req, res ) {
+	    res.partial( 'setup' );
+	} )
+	.get( '/admin', function( req, res ) {
+	    res.partial( 'admin', { 'locals': config } );
+	} )
+	.post( '/admin/sites', function( req, res ) {
+		res.contentType( 'json' );
+		var data;
+		switch ( req.body.action ) {
+			case 'run':
+				var run = req.body.run === 'true';
+				config.sites[req.body.site].run = run;
+				console.log( ( run ? 'Enabled' : 'Disabled' ) + ' ' + req.body.site );
+				data = { 'status': 'ok',  };
+				break;
+			default:
+				data = { 'status': 'error' };
+				break;
+		}
+		res.send( JSON.stringify( data ) )
+	} )
+	.get( '/display/random', function( req, res ) {
+		res.contentType( 'json' );
+		var siteArray = [];
+		for ( var name in config.sites ) {
+			siteArray.push( config.sites[name] );
+		}
+		var site = siteArray[Math.round( Math.random() * ( siteArray.length - 1 ) )];
+		var text = config.messages[Math.round( Math.random() * ( config.messages.length - 1 ) )];
+		res.send( JSON.stringify( {
+			'status': 'ok',
+			'url': site.url,
+			'time': site.time,
+			'text': text
+		} ) );
+	} )
+	.get( '/display/:frameset', function( req, res ) {
+	    res.partial( 'frameset', { 'locals': req.params } );
+	} )
+	.get( '/display/:frameset/:display', function( req, res ) {
+	    res.partial( 'display', { 'locals': req.params } );
+	} )
+	.listen( 8124 );
---
+++ beta/public/scripts/admin.js
@@ -0,0 +1,41 @@
+$(document).ready( function() {
+	$( '#tabs' ).tabs();
+	$( '#tabs li a' ).click( function() {
+		$(this).blur();
+	} );
+	$( '#displays a.blank' ).click( function( e ) {
+		$(this).blur();
+		e.preventDefault();
+		return false;
+	} );
+	$( '#menu button' ).button();
+	$( '#sites input[type=checkbox]' )
+		.each ( function() {
+			$(this).button( {
+				'text': false,
+				'icons': { 'primary': ( $(this).is( ':checked' ) ? 'ui-icon-check' : 'ui-icon-cancel' ) }
+			} );
+		} )
+		.click( function() {
+			$(this).button( { 'text': false, 'icons': { 'primary': 'ui-icon-clock' }, 'disabled': true } );
+			var value = $(this).is( ':checked' );
+			$.ajax( {
+				'type': 'POST',
+				'url': 'admin/sites',
+				'data': {
+					'action': 'run',
+					'run': value,
+					'site': $(this).attr( 'rel' )
+				},
+				'dataType': 'json',
+				'context': $(this),
+				'success': function( response ) {
+					$(this).button( {
+						'text': false,
+						'icons': { 'primary': ( value ? 'ui-icon-check' : 'ui-icon-cancel' ) },
+						'disabled': false
+					} );
+				}
+			} );
+		} );
+} );
