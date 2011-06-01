64a0ed7f269bc08066e7d489b2ec6591107792fc
Trevor Parscal <tparscal@users.mediawiki.org>
2011-06-01 16:47:28 +0000

    Updated to be compatible with latest version of express, which doesn't support call chaining on app menthods.

--- main.js
+++ main.js
@@ -4,58 +4,57 @@ var express = require( 'express' )
 
 var config = JSON.parse( fs.readFileSync( 'config.json' ) );
 
-app
-	.configure( function() {
-		app
-			.use( '/public', express.static( __dirname + '/public' ) )
-			.use( '/visualizations', express.static( __dirname + '/visualizations' ) )
-			.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) )
-			.use( express.bodyParser() )
-			.set( 'view engine', 'ejs' );
-	} )
-	.get( '/', function( req, res ) {
-	    res.partial( 'setup' );
-	} )
-	.get( '/admin', function( req, res ) {
-	    res.partial( 'admin', { 'locals': config } );
-	} )
-	.post( '/admin/sites', function( req, res ) {
-		res.contentType( 'json' );
-		var data;
-		switch ( req.body.action ) {
-			case 'run':
-				var run = req.body.run === 'true';
-				config.sites[req.body.site].run = run;
-				console.log( ( run ? 'Enabled' : 'Disabled' ) + ' ' + req.body.site );
-				data = { 'status': 'ok',  };
-				break;
-			default:
-				data = { 'status': 'error' };
-				break;
+app.configure( function() {
+	app
+		.use( '/public', express.static( __dirname + '/public' ) )
+		.use( '/visualizations', express.static( __dirname + '/visualizations' ) )
+		.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) )
+		.use( express.bodyParser() )
+		.set( 'view engine', 'ejs' );
+} );
+app.get( '/', function( req, res ) {
+    res.partial( 'setup' );
+} );
+app.get( '/admin', function( req, res ) {
+    res.partial( 'admin', { 'locals': config } );
+} );
+app.post( '/admin/sites', function( req, res ) {
+	res.contentType( 'json' );
+	var data;
+	switch ( req.body.action ) {
+		case 'run':
+			var run = req.body.run === 'true';
+			config.sites[req.body.site].run = run;
+			console.log( ( run ? 'Enabled' : 'Disabled' ) + ' ' + req.body.site );
+			data = { 'status': 'ok',  };
+			break;
+		default:
+			data = { 'status': 'error' };
+			break;
+	}
+	res.send( JSON.stringify( data ) )
+} );
+app.get( '/display/random', function( req, res ) {
+	res.contentType( 'json' );
+	var siteArray = [];
+	for ( var name in config.sites ) {
+		if ( config.sites[name].run ) {
+			siteArray.push( config.sites[name] );
 		}
-		res.send( JSON.stringify( data ) )
-	} )
-	.get( '/display/random', function( req, res ) {
-		res.contentType( 'json' );
-		var siteArray = [];
-		for ( var name in config.sites ) {
-			if ( config.sites[name].run ) {
-				siteArray.push( config.sites[name] );
-			}
-		}
-		var site = siteArray[Math.round( Math.random() * ( siteArray.length - 1 ) )];
-		var text = config.messages[Math.round( Math.random() * ( config.messages.length - 1 ) )];
-		res.send( JSON.stringify( {
-			'status': 'ok',
-			'url': site.url,
-			'time': site.time,
-			'text': text
-		} ) );
-	} )
-	.get( '/display/:frameset', function( req, res ) {
-	    res.partial( 'frameset', { 'locals': req.params } );
-	} )
-	.get( '/display/:frameset/:display', function( req, res ) {
-	    res.partial( 'display', { 'locals': req.params } );
-	} )
-	.listen( 8124 );
+	}
+	var site = siteArray[Math.round( Math.random() * ( siteArray.length - 1 ) )];
+	var text = config.messages[Math.round( Math.random() * ( config.messages.length - 1 ) )];
+	res.send( JSON.stringify( {
+		'status': 'ok',
+		'url': site.url,
+		'time': site.time,
+		'text': text
+	} ) );
+} );
+app.get( '/display/:frameset', function( req, res ) {
+    res.partial( 'frameset', { 'locals': req.params } );
+} );
+app.get( '/display/:frameset/:display', function( req, res ) {
+    res.partial( 'display', { 'locals': req.params } );
+} );
+app.listen( 8124 );
